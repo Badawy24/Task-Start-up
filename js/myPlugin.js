5e04b0cf735fd85da906da4cc24a7c1b02ba21d4
Abdelrahman <88353210+Badawy24@users.noreply.github.com>
2021-10-15 16:04:40 +0200

    Creat Nav Bar

---
+++ js/myPlugin.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 200){
+            $(".navbar-default").css("background-color","#1F1F1F");
+        } else {
+            $(".navbar-default").css("background-color","transparent");
+        }
+    });
+    $(".featured .feat-content").mouseenter(function() {
+        $(this).find(".cont").fadeIn();
+    });
+    $(".featured .feat-content").mouseleave(function() {
+        $(this).find(".cont").fadeOut();
+    });
+    $(".our-team .person").mouseenter(function() {
+        $(this).find(".image ul").slideDown();
+    });
+    $(".our-team .person").mouseleave(function() {
+        $(this).find(".image ul").slideUp();
+    });
+}); 
