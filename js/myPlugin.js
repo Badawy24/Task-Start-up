1ed9f32716e924fbb787ef4ba2f3d9e2ba33c592
Abdelrahman <88353210+Badawy24@users.noreply.github.com>
2021-10-16 22:05:03 +0200

    V0.2

--- js/myPlugin.js
+++ js/myPlugin.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
     $(window).on("scroll", function() {
-        if ($(window).scrollTop() > 400) {
-            $(".navbar").css("background-color", "#000");
+        if ($(window).scrollTop() > 150) {
+            $(".navbar").css("background-color", "rgba(0,0,0,0.9");
         } else {
             $(".navbar").css("background-color", "rgba(255,255,255,0)");
         }
+    });
+    $(".navbar-toggle").click(function() {
+        $("header .overlay .container").slideToggle(200);
     });
 
 });
